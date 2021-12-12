0952d8ee398c1bde2cb2b522472b5a9615acf4dd
yamodah <yassinedahlek@gmail.com>
2021-12-12 17:16:44 -0500

    added bootstrap, axios, api functions(no logic)

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,1 +1,14 @@
-const URL = process.env
+const URL = process.env.BASE_API_URL
+
+export async function listNotes(){
+    
+}
+export async function readNote(){
+
+}
+export async function changeNote(){
+
+}
+export async function deleteNote(){
+
+}
