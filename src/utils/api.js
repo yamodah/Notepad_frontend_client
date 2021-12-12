38cf538e2477d916aeaaea84ad3046f97268d43d
yamodah <yassinedahlek@gmail.com>
2021-12-12 17:46:21 -0500

    added logic for api functions

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,14 +1,18 @@
+import axios from "axios"
 const URL = process.env.BASE_API_URL
 
 export async function listNotes(){
-    
+    const data =  await axios.get(URL).catch(console.error)
+    return data
 }
-export async function readNote(){
-
+export async function readNote(noteId){
+    const data =  await axios.get(`${URL}/${noteId}`).catch(console.error)
+    return data
 }
-export async function changeNote(){
-
+export async function changeNote(updatedNote){
+    const data =  await axios.put(`${URL}/${updatedNote.id}`,updatedNote).catch(console.error)
+    return data 
 }
-export async function deleteNote(){
-
+export async function deleteNote(noteId){
+    await axios.delete(`${URL}/${noteId}`).catch(console.error)
 }
